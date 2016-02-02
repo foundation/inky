4c8a2b6d8d1eb14d91b096acea02b8e95b46da33
Tim Hartwick <tdhartwick2@gmail.com>
2016-02-02 14:55:55 -0800

    first pass of tests

--- test/grid-spec.js
+++ test/grid-spec.js
@@ -2,348 +2,288 @@ var Inky = require('../lib/inky');
 var cheerio = require('cheerio');
 var assert = require('assert');
 
+describe("the container", function() {
+  it("returns the correct container syntax", function() {
+    var inky = new Inky();
+    var $ = cheerio.load('<container></container>');
+
+    $ = inky.releaseTheKraken($);
+    compare($.html(), `
+        <table class="container">
+          <tbody>
+            <tr>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>`);
+  });
+});
+
+describe("centering", function() {
+  it("returns the correct centering syntax", function() {
+    var inky = new Inky();
+    var $ = cheerio.load('<center><table><tr><th></th></tr></table></center>');
+
+    $ = inky.releaseTheKraken($);
+    compare($.html(), `
+        <center>
+          <table class="center">
+            <tbody>
+              <tr>
+                <th></th>
+              </tr>
+            </tbody>
+          </table>
+        </center>`);
+  });
+});
+
+
 describe("the grid", function () {
   it("returns the correct row syntax", function() {
     var inky = new Inky();
-    var $ = cheerio.load('<center><row>This is a row!</row></center>');
+    var $ = cheerio.load('<row></row>');
 
     $ = inky.releaseTheKraken($);
     compare($.html(), `
-      <center>
         <table class="row">
           <tbody>
-            <tr>This is a row!</tr>
+            <tr></tr>
           </tbody>
-        </table>
-      </center>`);
-  })
-
-  it("returns the correct container syntax", function() {
-    var inky = new Inky();
-    var $ = cheerio.load('<center><container>This is a container!</container></center>');
-
-    $ = inky.releaseTheKraken($);
-    compare($.html(), `
-      <center>
-        <table class="container">
-          <tbody>
-            <tr>
-              <td>This is a container!</td>
-            </tr>
-          </tbody>
-        </table>
-      </center>`);
-  })
-
-  it("returns the correct column syntax", function () {
-    var inky = new Inky();
-    var $ = cheerio.load('<center><row><columns large="10"><h3>This is 10 Columns</h3></columns><columns large="2"><h3>This is 2 Columns</h3></columns></row></center>');
-
-    $ = inky.releaseTheKraken($);
-    compare($.html(), `
-      <center>
-        <table class="row">
-          <tbody>
-            <tr>
-              <td class="wrapper">
-                <table class="small-12 large-10 columns">
-                  <tr>
-                    <td>
-                      <h3>This is 10 Columns</h3>
-                    </td>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-              <td class="wrapper last">
-                <table class="small-12 large-2 columns">
-                  <tr>
-                    <td>
-                      <h3>This is 2 Columns</h3>
-                    </td>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </center>`);
+        </table>`);
   });
 
-  xit("returns the correct column syntax if it's one column", function() {
+  it("returns the correct sinlge column syntax", function () {
     var inky = new Inky();
-    var $ = cheerio.load('<input code here>');
+    var $ = cheerio.load('<columns large="12" small="12"></columns>');
 
     $ = inky.releaseTheKraken($);
     compare($.html(), `
-      <expected output here>
-    `);
+        <th class="columns small-12 large-12 first last">
+          <table >
+            <tr>
+              <th class="expander"></th>
+            </tr>
+          </table>
+        </th>`);
   });
 
-  xit("nests subcolumns correctly", function() {
+  it("returns the correct two column syntax", function () {
     var inky = new Inky();
-    var $ = cheerio.load('<center><row><columns large="6" small="12"><subcolumns large="4"><h3>Sub column 1</h3></subcolumns><subcolumns large="8"><h3>Sub column 2</h3></subcolumns></columns><columns large="6" small="12"><subcolumns large="6"><h3>Sub column 1.1</h3></subcolumns><subcolumns large="6"><h3>Sub column 1.2</h3></subcolumns></columns></row></center>');
+    var $ = cheerio.load('<columns large="6" small="12"></columns><columns large="6" small="12"></columns>');
 
     $ = inky.releaseTheKraken($);
     compare($.html(), `
-      <center>
-        <table class="row">
-          <tbody>
+        <th class="columns small-12 large-6 first">
+          <table >
             <tr>
-              <td class="wrapper">
-                <table class="small-12 large-6 columns">
-                  <td class="sub-columns small-12 large-4">
-                    <h3>Sub column 1</h3>
-                  </td>
-                  <td class="expander"></td>
-                  <td class="sub-columns last small-12 large-8">
-                    <h3>Sub column 2</h3>
-                  </td>
-                  <td class="expander"></td>
-                </table>
-              </td>
-              <td class="wrapper last">
-                <table class="small-12 large-6 columns">
-                  <td class="sub-columns small-12 large-6">
-                    <h3>Sub column 1.1</h3>
-                  </td>
-                  <td class="expander"></td>
-                  <td class="sub-columns last small-12 large-6">
-                    <h3>Sub column 1.2</h3>
-                  </td>
-                  <td class="expander"></td>
-                </table>
-              </td>
+              <th class="expander"></th>
             </tr>
-          </tbody>
-        </table>
-      </center>`);
+          </table>
+        </th>
+        <th class="columns small-12 large-6 last">
+          <table >
+            <tr>
+              <th class="expander"></th>
+            </tr>
+          </table>
+        </th>`);
+  });
+  
+  it("returns the correct three column syntax", function () {
+    var inky = new Inky();
+    var $ = cheerio.load('<columns large="4" small="12"></columns><columns large="4" small="12"></columns><columns large="4" small="12"></columns>');
+
+    $ = inky.releaseTheKraken($);
+    compare($.html(), `
+        <th class="columns small-12 large-4 first">
+          <table >
+            <tr>
+              <th class="expander"></th>
+            </tr>
+          </table>
+        </th>
+        <th class="columns small-12 large-4">
+          <table >
+            <tr>
+              <th class="expander"></th>
+            </tr>
+          </table>
+        </th>
+        <th class="columns small-12 large-4 last">
+          <table >
+            <tr>
+              <th class="expander"></th>
+            </tr>
+          </table>
+        </th>`);
   });
 
-  it("nests rows in containers", function() {
+  it("returns the correct offset syntax", function () {
     var inky = new Inky();
-    var $ = cheerio.load('<center><container><row>Row in container</row></container></center>');
+    var $ = cheerio.load('');
 
     $ = inky.releaseTheKraken($);
     compare($.html(), `
-      <center>
-        <table class="container">
-          <tbody>
-            <tr>
-              <td>
-                <table class="row">
-                  <tbody>
-                    <tr>Row in container</tr>
-                  </tbody>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </center>`);
-  })
+        <th class="columns small-4 small-offset-8 large-4 large-offset-8">
+        </th>`);
+  });
 
-  it("will place the 'last' class on the last column", function() {
-    var inky = new Inky();
-    var $ = cheerio.load('<center><row><columns large="10">.ten cols</columns><columns large="2">.two cols</columns></row></center>');
 
-    $ = inky.releaseTheKraken($);
-    compare($.html(), `
-      <center>
-        <table class="row">
-          <tbody>
-            <tr>
-              <td class="wrapper">
-                <table class="small-12 large-10 columns">
-                  <tr>
-                    <td>.ten cols</td>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-              <td class="wrapper last">
-                <table class="small-12 large-2 columns">
-                  <tr>
-                    <td>.two cols</td>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </center>`);
-
-  })
-
-  it("will place the 'last' class on a solo column", function() {
-    var inky = new Inky();
-    var $ = cheerio.load('<center><row><columns large="12">forever alone</columns></row></center>');
-
-    $ = inky.releaseTheKraken($);
-    compare($.html(), `
-      <center>
-        <table class="row">
-          <tbody>
-            <tr>
-              <td class="wrapper last">
-                <table class="small-12 large-12 columns">
-                  <tr>
-                    <td>forever alone</td>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </center>`);
-  })
-
-  it("handles deeply nested components", function() {
-    var inky = new Inky();
-    var $ = cheerio.load('<center><row><columns large="12"><p><callout>deep stuff</callout></p></columns></row></center>');
-
-    $ = inky.releaseTheKraken($);
-    compare($.html(), `
-      <center>
-        <table class="row">
-          <tbody>
-            <tr>
-              <td class="wrapper last">
-                <table class="small-12 large-12 columns">
-                  <tr>
-                    <td>
-                      <p>
-                        <td class="callout">deep stuff</td>
-                      </p>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </center>`);
-  })
-
+  //if it just has small, borrow from small for large
   it("automatically assigns large columns if no large attribute is assigned", function() {
     var inky = new Inky();
-    var $ = cheerio.load('<center><row><columns small="12"></columns><columns small="12"></columns><columns small="12"></columns></row></center>');
+    var $ = cheerio.load('<columns small="4"></columns><columns small="8"></columns>');
 
     $ = inky.releaseTheKraken($);
     compare($.html(), `
-      <center>
-        <table class="row">
-          <tbody>
-            <tr>
-              <td class="wrapper">
-                <table class="small-12 large-4 columns">
-                  <tr>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-              <td class="wrapper">
-                <table class="small-12 large-4 columns">
-                  <tr>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-              <td class="wrapper last">
-                <table class="small-12 large-4 columns">
-                  <tr>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </tbody>
+      <th class="columns small-4 large-4 first">
+        <table >
+          <tr>
+            <th class="expander"></th>
+          </tr>
         </table>
-      </center>`);
-  })
+      </th>
+      <th class="columns small-8 large-8 last">
+        <table >
+          <tr>
+            <th class="expander"></th>
+          </tr>
+        </table>
+      </th>`);
+  });
 
-  it("automatically assigns large columns if no large attribute is assigned, with custom grids", function() {
+  it("automatically assigns small columns as full width if only large defined", function() {
     var inky = new Inky();
-    var $ = cheerio.load('<center><row><columns></columns><columns></columns><columns></columns></row></center>');
+    var $ = cheerio.load('<columns large="4"></columns><columns large="8"></columns>');
 
     var opts = {
       grid: 15
     }
     $ = inky.releaseTheKraken($, opts);
     compare($.html(), `
-      <center>
-        <table class="row">
-          <tbody>
-            <tr>
-              <td class="wrapper">
-                <table class="small-15 large-5 columns">
-                  <tr>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-              <td class="wrapper">
-                <table class="small-15 large-5 columns">
-                  <tr>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-              <td class="wrapper last">
-                <table class="small-15 large-5 columns">
-                  <tr>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </tbody>
+      <th class="columns small-12 large-4 first">
+        <table >
+          <tr>
+            <th class="expander"></th>
+          </tr>
         </table>
-      </center>`);
-  })
+      </th>
+      <th class="columns small-12 large-8 last">
+        <table >
+          <tr>
+            <th class="expander"></th>
+          </tr>
+        </table>
+      </th>`);
+  });
+});
 
-  it("automatically assigns small columns as full width, with custom grids", function() {
+describe("the block grid", function() {
+  it("returns the correct block grid syntax", function() {
     var inky = new Inky();
-    var $ = cheerio.load('<center><row><columns></columns><columns></columns><columns></columns></row></center>');
+    var $ = cheerio.load('<block-grid up="4"></block-grid>');
 
-    var opts = {
-      grid: 15
-    }
-    $ = inky.releaseTheKraken($, opts);
+    $ = inky.releaseTheKraken($);
     compare($.html(), `
-      <center>
-        <table class="row">
-          <tbody>
-            <tr>
-              <td class="wrapper">
-                <table class="small-15 large-5 columns">
-                  <tr>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-              <td class="wrapper">
-                <table class="small-15 large-5 columns">
-                  <tr>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-              <td class="wrapper last">
-                <table class="small-15 large-5 columns">
-                  <tr>
-                    <td class="expander"></td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </center>`);
-  })
+      <table class="block-grid up-4">
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>`);
+  });
+});
+
+describe("buttons", function() {
+  it("returns the correct generic button syntax", function() {
+    var inky = new Inky();
+    var $ = cheerio.load('<button href="http://zurb.com"></button>');
+
+    $ = inky.releaseTheKraken($);
+    compare($.html(), `
+        <table class="button">
+          <tr>
+            <td>
+              <table>
+                <tr>
+                  <td>
+                    <a href="https://zurb.com">I am a button</a>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>`);
+  });
+
+  //adds the classes for complex buttons
+  it("returns the correct complex button syntax", function() {
+    var inky = new Inky();
+    var $ = cheerio.load('<button class="small alert expand" href="http://zurb.com"></button>');
+
+    $ = inky.releaseTheKraken($);
+    compare($.html(), `
+        <table class="button small alert expand">
+          <tr>
+            <td>
+              <table>
+                <tr>
+                  <td>
+                    <a href="https://zurb.com">I am a button</a>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>`);
+  });
+});
+
+describe("menus", function() {
+  it("returns the correct complete menu syntax", function() {
+    var inky = new Inky();
+    var $ = cheerio.load('<menu><item href="http://zurb.com">My Item 1</item><item href="http://zurb.com">My Item 2</item><item href="http://zurb.com">My Item 3</item></menu>');
+
+    $ = inky.releaseTheKraken($);
+    compare($.html(), `
+      <table class="menu">
+        <tr>
+          <td>
+            <a href="http://zurb.com">My Item 1</a>
+          </td>
+          <td>
+            <a href="http://zurb.com">My Item 2</a>
+          </td>
+          <td>
+            <a href="http://zurb.com">My Item 3</a>
+          </td>
+        </tr>
+      </table>`);
+  });
+
+  it("returns the correct menu wrapper with custom syntax", function() {
+    var inky = new Inky();
+    var $ = cheerio.load('<menu><item href="http://zurb.com">My Item 1</item><td><a href="http://zurb.com">My Item 2</a></td><item href="http://zurb.com">My Item 3</item></menu>');
+
+    $ = inky.releaseTheKraken($);
+    compare($.html(), `
+      <table class="menu">
+        <tr>
+          <td>
+            <a href="http://zurb.com">My Item 1</a>
+          </td>
+          <td>
+            <a href="http://zurb.com">My Item 2</a>
+          </td>
+          <td>
+            <a href="http://zurb.com">My Item 3</a>
+          </td>
+        </tr>
+      </table>`);
+  });
 });
 
 function compare(expected, actual) {
