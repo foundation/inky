a5649febc595fada0151f2597cd5e520062574dc
Geoff Kimball <geoff@zurb.com>
2016-02-03 15:47:14 -0800

    Remove unneeded test for <center> tag

--- test/grid.js
+++ test/grid.js
@@ -17,33 +17,6 @@ describe('Container', function() {
   });
 });
 
-describe('Centering', function() {
-  it('creates a properly centered table', function() {
-    var input = `
-      <center>
-        <table>
-          <tr>
-            <th></th>
-          </tr>
-        </table>
-      </center>
-    `;
-    var expected = `
-      <center>
-        <table class="center">
-          <tbody>
-            <tr>
-              <th></th>
-            </tr>
-          </tbody>
-        </table>
-      </center>
-    `;
-
-    compare(input, expected);
-  });
-});
-
 describe('Grid', function() {
   it('creates a row', function() {
     var input = '<row></row>';
@@ -106,21 +79,21 @@ describe('Grid', function() {
     `;
     var expected = `
       <th class="small-12 large-4 columns first">
-        <table >
+        <table>
           <tr>
             <th class="expander"></th>
           </tr>
         </table>
       </th>
       <th class="small-12 large-4 columns">
-        <table >
+        <table>
           <tr>
             <th class="expander"></th>
           </tr>
         </table>
       </th>
       <th class="small-12 large-4 columns last">
-        <table >
+        <table>
           <tr>
             <th class="expander"></th>
           </tr>
@@ -131,8 +104,8 @@ describe('Grid', function() {
     compare(input, expected);
   });
 
-  it('offsets a column', function() {
-    var input = '';
+  it.only('offsets a column', function() {
+    var input = '<columns class="small-offset-8"></columns>';
     var expected = `
       <th class="small-4 small-offset-8 large-4 large-offset-8 columns">
       </th>
