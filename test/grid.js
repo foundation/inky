c0597ffaacb2cef879100eb6f040dccf8588d8a7
Geoff Kimball <geoff@zurb.com>
2016-02-17 12:33:03 -0800

    Fix column syntax so that content sits adjacent to the expander tag

--- test/grid.js
+++ test/grid.js
@@ -61,11 +61,12 @@ describe('Grid', () => {
   });
 
   it('creates a single column with first and last classes', function () {
-    var input = '<columns large="12" small="12"></columns>';
+    var input = '<columns large="12" small="12">One</columns>';
     var expected = `
       <th class="small-12 large-12 columns first last">
         <table>
           <tr>
+            <th>One</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -77,13 +78,14 @@ describe('Grid', () => {
 
   it('creates two columns, one first, one last', function () {
     var input = `
-      <columns large="6" small="12"></columns>
-      <columns large="6" small="12"></columns>
+      <columns large="6" small="12">One</columns>
+      <columns large="6" small="12">Two</columns>
     `;
     var expected = `
       <th class="small-12 large-6 columns first">
         <table>
           <tr>
+            <th>One</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -91,6 +93,7 @@ describe('Grid', () => {
       <th class="small-12 large-6 columns last">
         <table>
           <tr>
+            <th>Two</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -102,14 +105,15 @@ describe('Grid', () => {
 
   it('creates 3+ columns, first is first, last is last', () => {
     var input = `
-      <columns large="4" small="12"></columns>
-      <columns large="4" small="12"></columns>
-      <columns large="4" small="12"></columns>
+      <columns large="4" small="12">One</columns>
+      <columns large="4" small="12">Two</columns>
+      <columns large="4" small="12">Three</columns>
     `;
     var expected = `
       <th class="small-12 large-4 columns first">
         <table>
           <tr>
+            <th>One</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -117,6 +121,7 @@ describe('Grid', () => {
       <th class="small-12 large-4 columns">
         <table>
           <tr>
+            <th>Two</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -124,6 +129,7 @@ describe('Grid', () => {
       <th class="small-12 large-4 columns last">
         <table>
           <tr>
+            <th>Three</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -134,11 +140,12 @@ describe('Grid', () => {
   });
 
   it('offsets a column', () => {
-    var input = '<columns class="small-offset-8"></columns>';
+    var input = '<columns class="small-offset-8">One</columns>';
     var expected = `
       <th class="small-offset-8 small-12 large-12 columns first last">
         <table>
           <tr>
+            <th>One</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -151,13 +158,14 @@ describe('Grid', () => {
   //if it just has small, borrow from small for large
   it('automatically assigns large columns if no large attribute is assigned', () => {
     var input = `
-      <columns small="4"></columns>
-      <columns small="8"></columns>
+      <columns small="4">One</columns>
+      <columns small="8">Two</columns>
     `;
     var expected = `
       <th class="small-4 large-4 columns first">
         <table>
           <tr>
+            <th>One</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -165,6 +173,7 @@ describe('Grid', () => {
       <th class="small-8 large-8 columns last">
         <table>
           <tr>
+            <th>Two</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -176,13 +185,14 @@ describe('Grid', () => {
 
   it('automatically assigns small columns as full width if only large defined', () => {
     var input = `
-      <columns large="4"></columns>
-      <columns large="8"></columns>
+      <columns large="4">One</columns>
+      <columns large="8">Two</columns>
     `;
     var expected = `
       <th class="small-12 large-4 columns first">
         <table>
           <tr>
+            <th>One</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -190,6 +200,7 @@ describe('Grid', () => {
       <th class="small-12 large-8 columns last">
         <table>
           <tr>
+            <th>Two</th>
             <th class="expander"></th>
           </tr>
         </table>
@@ -215,6 +226,7 @@ describe('Grid', () => {
                       </tbody>
                     </table>
                   </th>
+                  
                 </tr>
               </table>
             </th>
