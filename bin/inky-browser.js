08b5ec38727eb1944ba0c02ab7f29a51775079d9
Kevin Ball <kmball11@gmail.com>
2016-08-29 17:30:02 -0700

    Pass on browserified inky version

---
+++ bin/inky-browser.js
@@ -0,0 +1,39 @@
+var cheerio = require('cheerio');
+var Inky = require('../lib/inky');
+
+var inky;
+
+var setupInky = function(opts, cb) {
+  opts = opts || {};
+  opts.cheerio = Inky.mergeCheerioOpts(opts.cheerio);
+  if (typeof inky === 'undefined') {
+    inky = new Inky(opts);
+  }
+
+// This transform function takes in an element and calls a callback.
+  function transform(html, callback) {
+    var convertedHtml = inky.releaseTheKraken(html, opts.cheerio);
+    callback(null, convertedHtml);
+  }
+
+  cb(transform);
+}
+
+if(typeof(window) !== 'undefined') {
+  window.runInky = function(opts, elem) {
+    if(typeof(elem) === 'undefined') {
+      elem = opts;
+      opts = {};
+    }
+    setupInky(opts, function(transform) {
+      transform(elem.outerHTML, function(err, html) {
+        if(err === null) {
+          elem.outerHTML = html;
+        } else {
+          console.log(err);
+        }
+      });
+    });
+  }
+}
+
