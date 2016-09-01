42c1f467826c659945e52f774cfff11a4bdc52bd
Kevin Ball <kmball11@gmail.com>
2016-09-01 15:51:06 -0700

    Rip out multiline as unnecessary that breaks in browserified version

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,18 @@ var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
 
 gulp.task('browser', function() {
   var b = browserify({
     entries: 'bin/inky-browser.js',
-    debug: true
+    debug: false
   });
 
   return b.bundle()
     .pipe(source('inky-browser.js'))
     .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest('./dist/'));
 });
 
